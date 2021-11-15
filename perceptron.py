Fixed perceptron algorithm for stochastic gradient

perceptron.py
@@ -1,14 +1,15 @@
 from packages import *
 
 def perc(w, X):
-    # w is the weight vector
-    # X is the input matrix made by columns with input vectors
-    vector = X.loc[:,0]
-    label = X.loc[:,1]   
-    linear_sum = np.dot(w, vector)
-    prediction = sign(linear_sum)
-    error = label - prediction
-    return prediction, error
+
+    x_hom = np.vstack((np.ones(len(X.T[0]),dtype=int), X.T))
+
+    g = w @ x_hom
+    prediction = np.sign(g)
+
+    print (f'Prediction: {prediction}')
+    return prediction
+
     # output: binary vector composed by class labels 1 & -1
 
 def percTrain(X, t, maxIts, online):
@@ -17,7 +18,7 @@ def percTrain(X, t, maxIts, online):
 
     ## homogenize feature vectors
     ## needed to be transposed due to upcoming matrix multiplication
-    x_hom = np.vstack((np.ones(len(X[0]),dtype=int), X)).T
+    x_hom = np.vstack((np.ones(len(X.T[0]),dtype=int), X.T))
     w = np.zeros(len(x_hom))
    
     
@@ -32,3 +33,4 @@ def percTrain(X, t, maxIts, online):
 
     print (f'{epoch} epochs needed. w = {w}')
     return w
+    # output: trained weight vector