f7bf0d8691ae5e5441da963d6fe6acc364c157bb
danielhp97 <danielhp1234@gmail.com>
2021-11-25 18:11:27 +0100

    added lms online basic algorithm

--- part2/lms.py
+++ part2/lms.py
@@ -1,9 +1,12 @@
+import pandas as pd
+import numpy as np
 #lms regresssion
 
 # minimize F(w)
 
 w= [0.1,0.2,0.3,0.4,0.5] # weight vector
-x=[0.52,0.53,0.54,0.44,0.34] # points (x,y)
+x=[0.52,0.53,0.54,0.44,0.34]
+t=[1,1,-1,1,-1] # points (x,y)
 # f(w,n)
 def f(w,x):
     first_sum = 0
@@ -13,6 +16,34 @@ def f(w,x):
 result = f(w,x)
 
 #w1g1(xn) + w2g2(xn) + · · · + wIgI(xn)
+
+
+# we need to replicate online algorith (same rule for weight) but apply this rule to calculate weight vector
+
+def percTrain_lms(X,t,maxIts):
+    stop=0
+    i=0
+    tol = 1e-4 # tolerance value
+    ## homogenize feature vectors
+    ## needed to be transposed due to upcoming matrix multiplication
+    x_hom = np.vstack((np.ones(len(X[0]),dtype=int), X))
+    w = np.zeros(len(x_hom))
+    while stop==0:
+        for epoch in range(1,maxIts):
+            i = epoch
+            # we can add random  initialization:
+            # random.randint(0,len(X))
+            for j in range(len(t)):
+                if w.T @ (x_hom[:,j] * t[j]) <= 0:
+                    w += pow(w[j]*x^[j],2)
+                    stop=1
+                    if stop==1:
+                        break
+    return w
+
+
+result_weight_vector = percTrain_lms(x,t,10)
+
 
 
 
